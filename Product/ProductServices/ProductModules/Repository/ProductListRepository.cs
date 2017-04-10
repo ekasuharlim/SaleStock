27cb22ec725c923cc2c1567bf9cb598b9107ea6c
ekasuharlim <eka.suharlim>
2017-04-10 22:50:12 +0800

    add ui

--- Product/ProductServices/ProductModules/Repository/ProductListRepository.cs
+++ Product/ProductServices/ProductModules/Repository/ProductListRepository.cs
@@ -12,31 +12,101 @@ namespace ProductModules.Repository
         public ProductRepository()
         {
             m_productData = new List<Product>();
-            m_productData.Add(new Product { ProductId = 1, ArticleNo = "ABC", Name = "SHIRT A", ImageName = "SHRIT1", Description = "DESC",Price = 1000});
-            m_productData.Add(new Product { ProductId = 2, ArticleNo = "ABC", Name = "SHIRT B", ImageName = "SHRIT1", Description = "DESC", Price = 2000 });
-            m_productData.Add(new Product { ProductId = 3, ArticleNo = "ABC", Name = "SHIRT C", ImageName = "SHRIT1", Description = "DESC", Price = 3000 });
-            m_productData.Add(new Product { ProductId = 4, ArticleNo = "ABC", Name = "SHIRT D", ImageName = "SHRIT1", Description = "DESC", Price = 4000 });
-            m_productData.Add(new Product { ProductId = 5, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 5000 });
-            m_productData.Add(new Product { ProductId = 6, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 6000 });
-            m_productData.Add(new Product { ProductId = 7, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 7000 });
-            m_productData.Add(new Product { ProductId = 8, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC",Price = 8000 });
-            m_productData.Add(new Product { ProductId = 9, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 9000 });
-            m_productData.Add(new Product { ProductId = 10, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 10000 });
-            m_productData.Add(new Product { ProductId = 11, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 11000 });
-            m_productData.Add(new Product { ProductId = 12, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 12000 });
-            m_productData.Add(new Product { ProductId = 13, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 13000 });
-            m_productData.Add(new Product { ProductId = 14, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 14000 });
-            m_productData.Add(new Product { ProductId = 15, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 15000 });
-            m_productData.Add(new Product { ProductId = 16, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 16000 });
-            m_productData.Add(new Product { ProductId = 17, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 17000 });
-            m_productData.Add(new Product { ProductId = 18, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 18000 });
-            m_productData.Add(new Product { ProductId = 19, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 19000 });
-            m_productData.Add(new Product { ProductId = 20, ArticleNo = "ABC", Name = "SHIRT", ImageName = "SHRIT1", Description = "DESC", Price = 20000 });
+            m_productData.Add(new Product
+            {
+                ProductId = 1,
+                ArticleNo = "A-2121",
+                Name = "Mickey",
+                ImageName = "A-2121.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 32500
+            });
+            m_productData.Add(new Product
+            {
+                ProductId = 2,
+                ArticleNo = "A-2122",
+                Name = "Tom&Jerry",
+                ImageName = "A-2122.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 32000
+            });
+            m_productData.Add(new Product
+            {
+                ProductId = 3,
+                ArticleNo = "A-2131",
+                Name = "Joe Tiger",
+                ImageName = "A-2131.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 32000
+            });
+            m_productData.Add(new Product
+            {
+                ProductId = 4,
+                ArticleNo = "A-2141",
+                Name = "Tom&Jerry",
+                ImageName = "A-2141.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 10000
+            });
+            m_productData.Add(new Product
+            {
+                ProductId = 5,
+                ArticleNo = "A-2002",
+                Name = "Tom&Jerry",
+                ImageName = "A-2002.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 12000
+            });
+            m_productData.Add(new Product
+            {
+                ProductId = 6,
+                ArticleNo = "A-2011",
+                Name = "Giraffe Safari",
+                ImageName = "A-2011.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 30000
+            });
+            m_productData.Add(new Product
+            {
+                ProductId = 7,
+                ArticleNo = "A-2012",
+                Name = "Giraffe Zoo",
+                ImageName = "A-2012.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 18000
+            });
+            m_productData.Add(new Product
+            {
+                ProductId = 8,
+                ArticleNo = "A-1632",
+                Name = "Apple",
+                ImageName = "A-1632.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 19000
+            });
+            m_productData.Add(new Product
+            {
+                ProductId = 9,
+                ArticleNo = "A-1634",
+                Name = "Princess",
+                ImageName = "A-1634.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 12000
+            });
+            m_productData.Add(new Product
+            {
+                ProductId = 10,
+                ArticleNo = "A-1633",
+                Name = "Rabbit",
+                ImageName = "A-1633.jpg",
+                Description = "Baju : LD = 27 cm P = 36 cm Celana : Pinggang Karet P = 28 cm",
+                Price = 32000
+            });
         }
         public IEnumerable<Product> GetPagedItem(int pageNo, int count)
         {
             return m_productData.Skip((pageNo - 1) * count).Take(count);
-            
+
         }
 
         public Product GetProductDetail(int productId)
